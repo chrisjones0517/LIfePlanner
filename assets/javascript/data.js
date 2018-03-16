7a59e77e61284d0d495a138cea5d5116ab7bb00e
Chris Jones <chrisjones0517@gmail.com>
2018-03-16 17:51:52 -0500

    data setup - early phase

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,23 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var database = firebase.database();
+    var ref = database.ref('users');
+    ref.on('value', dataChange, errData);
+
 
     var container = document.getElementById('visualization');
-    var items = [
-      {x: '2014-06-11', y: 10},
-      {x: '2014-06-12', y: 25},
-      {x: '2014-06-13', y: 30},
-      {x: '2014-06-14', y: 10},
-      {x: '2014-06-15', y: 15},
-      {x: '2014-06-16', y: 30}
-    ];
-  
-    var dataset = new vis.DataSet(items);
-    var options = {
-      start: '2014-06-10',
-      end: '2014-06-18'
-    };
-    var graph2d = new vis.Graph2d(container, dataset, options);
+
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        var userName = $('#userName').val();
+        var calThisMeal = $('#calThisMeal').val();
+        var data = {
+            userID: userName,
+            calories: calThisMeal
+        }
+        ref.push(data);
+        console.log(data);
 
 
+    });
 
+    function dataChange(data) {
+        var dataArr = [];
+
+        data.forEach(function (childData) {
+            var item = childData.val();
+            item.key = childData.key;
+            dataArr.push(item);
+
+            var items = [
+                { x: '2014-06-11', y: 10 },
+                { x: '2014-06-12', y: 25 },
+                { x: '2014-06-13', y: 30 },
+                { x: '2014-06-14', y: 10 },
+                { x: '2014-06-15', y: 15 },
+                { x: '2014-06-16', y: 30 }
+            ];
+
+            var dataset = new vis.DataSet(items);
+            var options = {
+                start: '2014-06-10',
+                end: '2014-06-18'
+            };
+            var graph2d = new vis.Graph2d(container, dataset, options);
+
+        });
+
+        return dataArr
+    }
+
+    function errData(error) {
+        console.log('Error!');
+        console.log(error);
+    }
 
 });
+
