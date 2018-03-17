7ffd2aedbefe9b2369b5ff195adf8c006cd02ee4
Chris Jones <chrisjones0517@gmail.com>
2018-03-16 21:58:32 -0500

    change of plans

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,29 @@
 $(document).ready(function () {
 
-    
+
 
     $('#submit').on('click', function (e) {
         e.preventDefault();
         $("#data").empty();
         var query = $('#search').val();
 
-        $.ajax({
-            url: `https://developers.zomato.com/api/v2.1/search?q=${query}`,
-            type: 'POST',
-            headers: {
-                'user-key': 'a935c21d6210ae554c1a824b76165e48'
-            },
-            data: {
-                format: 'json'
-            },
-            success: function (data) {
-                var rests = data.restaurants;
-                console.log(rests);
-
-                for (var i = 0; i < rests.length; i++) {
-                    $('#data').append(`
-                <ul>
-                    <li>${rests[i].restaurant.average_cost_for_two}</li>
-                    <li>${rests[i].restaurant.cuisines}</li>
-                    <li>${rests[i].restaurant.location.address}</li>
-                    <li><a href="#">${rests[i].restaurant.menu_url}</a></li>
-                    <li>${rests[i].restaurant.name}</li>
-                    <li><a href="#">${rests[i].restaurant.photos_url}</a></li>
-                    <li>${rests[i].restaurant.price_range}</li>
-                </ul>
-                `);
-                }
-                //   $('#data').text(JSON.stringify(data.restaurants));
-                console.log(data);
-            },
-            error: function (error) {
-                console.log(error);
-            }
 
 
-            
+        var url = "https://api.datausa.io/api/?show=geo&sumlevel=state&required=avg_wage";
+
+        d3.json(url, function (json) {
+
+            var data = json.data.map(function (data) {
+                return json.headers.reduce(function (obj, header, i) {
+                    obj[header] = data[i];
+                    return obj;
+                    console.log(obj);
+                }, {});
+            });
 
         });
 
     });
-
-    
-
-    // https://developers.zomato.com/api/v2.1/search?q=tacos&count=20
-
-    // a935c21d6210ae554c1a824b76165e48
-
-    // fatSecret api key c40057b455724db2804c75ec57a9f9d6
-
-    // fatSecret shared secret 991cdd08c3354611b1d5047ab42bbbd0
+  
 
 });
