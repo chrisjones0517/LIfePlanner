a0609f058540031fb1a2b3507da5525026fae6d2
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-03-18 17:08:52 -0500

    google timezone call from lt and ln

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,8 +32,13 @@ $(document).ready(function () {
         console.log("wasclicked")
         WaetherCall()
     })
+
+
+    //for pulling correct temp
     var units = 'imperial';
+    //weather function
     function WaetherCall() {
+        //will take input from search based on lat and long
         var inputWeather = "New York"   //$("#search-input").val().trim() ;
         //api.openweathermap.org/data/2.5/forecast?lat=35&lon=139; getting info from Mohammed lt return 
         var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputWeather + ",us&APPID=eeda0b646e014b160ccbce009bb655ef";
@@ -45,6 +50,7 @@ $(document).ready(function () {
                 units: units
             }
         }).then(function (data) {
+            //taking all infor from api
             console.log(data)
             lat = data.city.coord.lat;
             lon = data.city.coord.lon;
@@ -56,33 +62,28 @@ $(document).ready(function () {
             icon = data.list[0].weather[0].icon;
             console.log(lat, lon)
             console.log(city, cityPop)
-            console.log(lowF, highF,description)
+            console.log(lowF, highF, description)
+            //appending info
             $('#city').html('city: ' + city)
             $('#cityPop').html('cityPop: ' + cityPop);
             $('#description').html('description: ' + description)
             $('#highF').html('highF: ' + highF);
             $('#lowF').html('lowF: ' + lowF);
-                var times_Stamp = (Math.round((new Date().getTime())/1000)).toString(); 
-                $.ajax({
-                 url:"https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lon + "&timestamp=" + times_Stamp,
-            
-                 type: "POST",
-                }).done(function(response){
-              
-                 var Cur_Date = new Date();
-                 var UTC = Cur_Date.getTime() + (Cur_Date.getTimezoneOffset() * 60000);
-                 var Loc_Date = new Date(UTC + (1000*response.rawOffset) + (1000*response.dstOffset));
-                      $("#timeOfLocation").html('Current Time : ' + Loc_Date);
-                    
-                  }); 
+            //based on weather lat and lon grabbing time information
+            var times_Stamp = (Math.round((new Date().getTime()) / 1000)).toString();
+            $.ajax({
+                url: "https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lon + "&timestamp=" + times_Stamp,
+                type: "POST",
+            }).done(function (response) {
+                var Cur_Date = new Date();
+                var UTC = Cur_Date.getTime() + (Cur_Date.getTimezoneOffset() * 60000);
+                var Loc_Date = new Date(UTC + (1000 * response.rawOffset) + (1000 * response.dstOffset));
+                $("#timeOfLocation").html('Current Time : ' + Loc_Date);
+
+            });
         });
-        
+
     }
 
-    // var Latitude  = 6.0535185;
-    // var Longitude = 80.22097729999996;
-    // getTimeUsingLatLng(Latitude,Longitude);
-     
-   
-    
+
 });
