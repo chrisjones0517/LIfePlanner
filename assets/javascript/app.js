370ec3434cd1f0a64e46aa77c5ba5d9638b0522b
Chris Jones <chrisjones0517@gmail.com>
2018-03-18 22:30:03 -0500

    more api additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
         var queryUrl = `https://api.careeronestop.org/v1/occupation/NzX2rM28B8dZLR3/${occupation}/y/0/10`;
 
-        
+
         $.ajax({
             url: queryUrl,
             dataType: 'json',
@@ -27,9 +27,12 @@ $(document).ready(function () {
                 //   console.log(response.OccupationList)
                 //  console.log(occTitle);
                 //  console.log(occCode);
+            },
+            error: function(request,status,errorThrown) {
+                console.log('This is where the error will be output to the user.');
             }
         }).then(function () {
-            
+
             $.ajax({
                 url: `https://api.careeronestop.org/v1/occupation/NzX2rM28B8dZLR3/${occCode}/${cityStateZip}?training=false&interest=false&videos=false&tasks=false&dwas=false&wages=true&alternateOnetTitles=false&projectedEmployment=true&ooh=false&stateLMILinks=false&relatedOnetTitles=false&skills=false&knowledge=false&ability=false&trainingPrograms=false`,
                 dataType: 'json',
@@ -49,16 +52,20 @@ $(document).ready(function () {
                     for (var i = 0; i < localWages.length; i++) {
                         if (localWages[i].RateType === 'Annual') {
                             console.log('City median income: ' + localWages[i].Median);
-                          //  $('#someHTMLid').text(localWages[i].Median);
+                            //  $('#someHTMLid').text(localWages[i].Median);
                         }
                     }
                     for (var i = 0; i < natWages.length; i++) {
                         if (natWages[i].RateType === 'Annual') {
                             console.log('National median income: ' + natWages[i].Median);
-                         //   $('#someHTMLid').text(natWages[i].Median);
+                            //   $('#someHTMLid').text(natWages[i].Median);
                         }
                     }
+                    console.log('US Median Per Capita Income: $29,829');
                     console.log(myRoot);
+                },
+                error: function(request,status,errorThrown) {
+                    console.log('This is where my error will go to be ouput to the user.');
                 }
             });
         });
@@ -74,14 +81,9 @@ $(document).ready(function () {
         console.log("wasclicked")
         WaetherCall()
     })
-
-
-    //for pulling correct temp
     var units = 'imperial';
-    //weather function
     function WaetherCall() {
-        //will take input from search based on lat and long
-        var inputWeather = "New York"   //$("#search-input").val().trim() ;
+        var inputWeather = "Houston"   //$("#search-input").val().trim() ;
         //api.openweathermap.org/data/2.5/forecast?lat=35&lon=139; getting info from Mohammed lt return 
         var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputWeather + ",us&APPID=eeda0b646e014b160ccbce009bb655ef";
         $.ajax({
@@ -91,8 +93,8 @@ $(document).ready(function () {
                 cnt: 16,
                 units: units
             }
+
         }).then(function (data) {
-            //taking all infor from api
             console.log(data)
             lat = data.city.coord.lat;
             lon = data.city.coord.lon;
@@ -101,31 +103,28 @@ $(document).ready(function () {
             highF = Math.round(data.list[0].main.temp_max) + '°';
             lowF = Math.round(data.list[0].main.temp_min) + '°';
             description = data.list[0].weather[0].description;
-            icon = data.list[0].weather[0].icon;
             console.log(lat, lon)
             console.log(city, cityPop)
             console.log(lowF, highF, description)
-            //appending info
-            $('#city').html('city: ' + city)
-            $('#cityPop').html('cityPop: ' + cityPop);
-            $('#description').html('description: ' + description)
-            $('#highF').html('highF: ' + highF);
-            $('#lowF').html('lowF: ' + lowF);
-            //based on weather lat and lon grabbing time information
-            var times_Stamp = (Math.round((new Date().getTime()) / 1000)).toString();
-            $.ajax({
-                url: "https://maps.googleapis.com/maps/api/timezone/json?location=" + lat + "," + lon + "&timestamp=" + times_Stamp,
-                type: "POST",
-            }).done(function (response) {
-                var Cur_Date = new Date();
-                var UTC = Cur_Date.getTime() + (Cur_Date.getTimezoneOffset() * 60000);
-                var Loc_Date = new Date(UTC + (1000 * response.rawOffset) + (1000 * response.dstOffset));
-                $("#timeOfLocation").html('Current Time : ' + Loc_Date);
-
-            });
         });
+
 
     }
 
+    var climateUrl = `https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/`;
+
+    $('#climate').on('click', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: climateUrl,
+            data: {CITY:'houston,tx'},
+            method: 'GET',
+            headers: { token: 'WWKoJVmRVKlQKXOsSHFiQZXozlzIBzJY' }
+        }).then(function(response) {
+            console.log(response);
+        });
+    });
+
 
 });
+
